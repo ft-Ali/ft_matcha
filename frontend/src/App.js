a82ac6ce70bddbac072a57362dea5db53012c5d6
ft-Ali <alsiavos@student.42.fr>
2025-05-28 16:46:06 +0200

    Setup docker and nginx config files (to be reviewed and improved)

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from './components/profil';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
