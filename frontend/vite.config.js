fd23fccca54c095ecc64b24d3b848d264e976b60
Ali Siavoshi Fard <alsiavos@f1br4s8.clusters.42paris.fr>
2025-06-13 18:26:34 +0200

    Need debug on the docker + vitejs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'app/build',
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-        profile: 'src/components/profil.jsx',
-        home: 'src/components/home.jsx',
-      },
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
-    emptyOutDir: true,
+    outDir: '../nginx/build', 
+    emptyOutDir: true
   },
-})
+  server: {
+    host: true,              
+    port: 5173
+  }
+});
