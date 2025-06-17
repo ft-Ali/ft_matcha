501d656447b544af57ad852fa1133cf8364ffd5f
Ali Siavoshi Fard <alsiavos@f6r4s11.clusters.42paris.fr>
2025-06-17 18:04:23 +0200

    vite.js setup correctly + project is in dev mode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../nginx/build', 
+    outDir: 'build', 
     emptyOutDir: true
   },
   server: {
     host: true,              
-    port: 5173
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
   }
 });
